6d89ec7b661fda435c140539eea1f91aa3c84b0b
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2023-09-27 13:55:07 -0400

    switch to class to pull in filecache

--- index.js
+++ index.js
@@ -14,119 +14,124 @@ const MODULE_IGNORES = [
 const RESOLVE_DIRS = ["app", "packages"];
 const RESOLVE_EXTENSIONS = ["js", "jsx", "ts", "tsx", "json"];
 
-const filecache = new Map();
-
-function findModules(...patterns) {
-  return glob
-    .sync(patterns, { nodir: true, ignore: MODULE_IGNORES })
-    .filter((file) => MODULE_EXTENSIONS.includes(path.extname(file)));
-}
-
-function fileExists(filename) {
-  if (filecache.has(filename)) return filecache.get(filename);
-  let exists;
-  try {
-    exists = fs.statSync(filename).isFile();
-  } catch {
-    exists = false;
+class Searcher {
+  constructor() {
+    this.filecache = new Map();
   }
-  filecache.set(filename, exists);
-  return exists;
-}
 
-function resolveRelativeImportPath(directory, importPath) {
-  // eg. directory/filename
-  const resolvedExact = path.join(directory, importPath);
-  if (fileExists(resolvedExact)) return resolvedExact;
-
-  for (const extension of RESOLVE_EXTENSIONS) {
-    // eg. directory/filename.ext
-    const resolvedWithExtension = path.join(
-      directory,
-      `${importPath}.${extension}`
-    );
-    if (fileExists(resolvedWithExtension)) return resolvedWithExtension;
-
-    // eg. directory/filename/index.ext
-    const resolvedIndex = path.join(
-      directory,
-      importPath,
-      `index.${extension}`
-    );
-    if (fileExists(resolvedIndex)) return resolvedIndex;
+  findModules(patterns) {
+    return glob
+      .sync(patterns, { nodir: true, ignore: MODULE_IGNORES })
+      .filter((file) => MODULE_EXTENSIONS.includes(path.extname(file)));
   }
-  return undefined;
-}
-
-function resolveImportPath(filename, importPath) {
-  if (importPath.startsWith(".")) {
-    const filedir = path.dirname(filename);
-    return resolveRelativeImportPath(filedir, importPath);
+  
+  fileExists(filename) {
+    if (this.filecache.has(filename)) return this.filecache.get(filename);
+    let exists;
+    try {
+      exists = fs.statSync(filename).isFile();
+    } catch {
+      exists = false;
+    }
+    this.filecache.set(filename, exists);
+    return exists;
   }
-  for (const dir of RESOLVE_DIRS) {
-    const result = resolveRelativeImportPath(dir, importPath);
-    if (result) return result;
+  
+  resolveRelativeImportPath(directory, importPath) {
+    // eg. directory/filename
+    const resolvedExact = path.join(directory, importPath);
+    if (this.fileExists(resolvedExact)) return resolvedExact;
+  
+    for (const extension of RESOLVE_EXTENSIONS) {
+      // eg. directory/filename.ext
+      const resolvedWithExtension = path.join(
+        directory,
+        `${importPath}.${extension}`
+      );
+      if (this.fileExists(resolvedWithExtension)) return resolvedWithExtension;
+  
+      // eg. directory/filename/index.ext
+      const resolvedIndex = path.join(
+        directory,
+        importPath,
+        `index.${extension}`
+      );
+      if (this.fileExists(resolvedIndex)) return resolvedIndex;
+    }
+    return undefined;
   }
-  return undefined;
-}
-
-function getImports(filename) {
-  try {
-    const file = fs.readFileSync(filename, { encoding: "utf8" });
-    const ast = parse(file, {
-      sourceType: "module",
-      plugins: ["jsx", "typescript", "decorators"],
+  
+  resolveImportPath(filename, importPath) {
+    if (importPath.startsWith(".")) {
+      const filedir = path.dirname(filename);
+      return this.resolveRelativeImportPath(filedir, importPath);
+    }
+    for (const dir of RESOLVE_DIRS) {
+      const result = this.resolveRelativeImportPath(dir, importPath);
+      if (result) return result;
+    }
+    return undefined;
+  }
+  
+  getImports(filename) {
+    try {
+      const file = fs.readFileSync(filename, { encoding: "utf8" });
+      const ast = parse(file, {
+        sourceType: "module",
+        plugins: ["jsx", "typescript", "decorators"],
+      });
+      const imports = new Set();
+      traverse(ast, {
+        // import x from './x';
+        ImportDeclaration: (nodePath) => {
+          if (!nodePath.node.source || !nodePath.node.source.value) return;
+          const rawPath = nodePath.node.source.value;
+          const importPath = this.resolveImportPath(filename, rawPath);
+          if (importPath) imports.add(importPath);
+        },
+        // import('./x')
+        ImportExpression: (nodePath) => {
+          if (!nodePath.node.source) return;
+          // Ignore constant template literals, import(`.x`);
+          if (nodePath.node.source.type === "TemplateLiteral") return;
+          const rawPath = nodePath.node.source.value;
+          const importPath = this.resolveImportPath(filename, rawPath);
+          if (importPath) imports.add(importPath);
+        },
+        // export { x } from './x';
+        ExportNamedDeclaration: (nodePath) => {
+          if (!nodePath.node.source || !nodePath.node.source.value) return;
+          const rawPath = nodePath.node.source.value;
+          const importPath = this.resolveImportPath(filename, rawPath);
+          if (importPath) imports.add(importPath);
+        },
+        ExportAllDeclaration: (nodePath) => {
+          if (!nodePath.node.source || !nodePath.node.source.value) return;
+          const rawPath = nodePath.node.source.value;
+          const importPath = this.resolveImportPath(filename, rawPath);
+          if (importPath) imports.add(importPath);
+        },
+      });
+      return imports;
+    } catch (error) {
+      console.error(`Error while parsing: ${filename}`);
+      throw error;
+    }
+  }
+  
+  getAllImports(filenames) {
+    const importPaths = new Set();
+    filenames.forEach((filename) => {
+      this.getImports(filename).forEach((importPath) => {
+        importPaths.add(importPath);
+      });
     });
-    const imports = new Set();
-    traverse(ast, {
-      // import x from './x';
-      ImportDeclaration: (nodePath) => {
-        if (!nodePath.node.source || !nodePath.node.source.value) return;
-        const rawPath = nodePath.node.source.value;
-        const importPath = resolveImportPath(filename, rawPath);
-        if (importPath) imports.add(importPath);
-      },
-      // import('./x')
-      ImportExpression: (nodePath) => {
-        if (!nodePath.node.source) return;
-        // Ignore constant template literals, import(`.x`);
-        if (nodePath.node.source.type === "TemplateLiteral") return;
-        const rawPath = nodePath.node.source.value;
-        const importPath = resolveImportPath(filename, rawPath);
-        if (importPath) imports.add(importPath);
-      },
-      // export { x } from './x';
-      ExportNamedDeclaration: (nodePath) => {
-        if (!nodePath.node.source || !nodePath.node.source.value) return;
-        const rawPath = nodePath.node.source.value;
-        const importPath = resolveImportPath(filename, rawPath);
-        if (importPath) imports.add(importPath);
-      },
-      ExportAllDeclaration: (nodePath) => {
-        if (!nodePath.node.source || !nodePath.node.source.value) return;
-        const rawPath = nodePath.node.source.value;
-        const importPath = resolveImportPath(filename, rawPath);
-        if (importPath) imports.add(importPath);
-      },
-    });
-    return imports;
-  } catch (error) {
-    console.error(`Error while parsing: ${filename}`);
-    throw error;
+    return importPaths;
   }
-}
-
-function getAllImports(filenames) {
-  const importPaths = new Set();
-  filenames.forEach((filename) => {
-    getImports(filename).forEach((importPath) => {
-      importPaths.add(importPath);
-    });
-  });
-  return importPaths;
 }
 
 function main() {
+  const searcher = new Searcher();
   yargs
     .scriptName("imported")
     .usage("$0 <cmd> [args]")
@@ -142,8 +147,8 @@ function main() {
         });
       },
       function (argv) {
-        const allFilenames = findModules(...argv.pattern);
-        Array.from(getAllImports(allFilenames))
+        const allFilenames = searcher.findModules(argv.pattern);
+        Array.from(searcher.getAllImports(allFilenames))
           .sort()
           .forEach((importPath) => {
             console.log(importPath);
@@ -167,9 +172,9 @@ function main() {
         });
       },
       function (argv) {
-        const allFilenames = findModules(...argv.pattern);
+        const allFilenames = searcher.findModules(argv.pattern);
         for (const filename of allFilenames.sort()) {
-          const importPaths = getImports(filename);
+          const importPaths = searcher.getImports(filename);
           if (importPaths.has(argv.file)) console.log(filename);
         }
       }
